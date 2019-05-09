c96dea10876188040db33f51550dd7b23f354fff
mromero <maria.romero@news.co.uk>
2019-05-09 15:55:23 +0100

    A2:wip getting form to submit

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 var express = require('express')
 var app = express()
 var path = require('path');
+// app.use(express.urlencoded())
 
 const port = 3000
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 })
+
+app.post('/submit-form', (req, res) => {
+  const article = req.body.article
+  console.log(article)
+
+}).on("error", (err) => {
+  console.log("Error: " + err.message);
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
