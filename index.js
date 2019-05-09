2fcc4c3bec760528966304c143035df633257a93
mromero <maria.romero@news.co.uk>
2019-05-09 14:01:39 +0100

    A1: form created

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var express = require('express')
 var app = express()
+var path = require('path');
+
 const port = 3000
 
 app.get('/', function (req, res) {
-  res.send('hello world')
+  res.sendFile(path.join(__dirname + '/index.html'));
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
