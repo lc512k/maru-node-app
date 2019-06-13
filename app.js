e233e6f3123e0fe76fea8179046b59a51464ed22
mromero <maria.romero@news.co.uk>
2019-06-13 14:45:59 +0100

    A3: calling FT API but getting 403

--- app.js
+++ app.js
@@ -12,23 +12,25 @@ app.get('/', function (req, res) {
 
 app.post('/submit-form', (req, res) => {
   const article = req.body.article;
+  var url =  'http://api.ft.com/content/search/v1?';
+
   const callConfig = {
-    method: 'get',
-    url: 'http://api.ft.com/content/search/v1?',
-    responseType: 'stream',
     headers: {
-      'X-Api-Key':'',
+      'X-Api-Key':'59cbaf20e3e06d3565778e7bde0b7e265f5844ddb68d827a9f3b7879',
       'Content-Type':'application/json'
-  },
-  queryString: article,
+    },
+    Connection:'keep-alive',
+
+    queryString: article,
   };
-  res = await axios(callConfig);
 
-  console.log("res.status " + res.status);
-  console.log("res.data " + res.data);
-
-}).on("error", (err) => {
-  console.log("Error: " + err.message);
+  axios.post(url, callConfig)
+    .then(function (response) {
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
