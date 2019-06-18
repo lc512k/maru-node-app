c24a4ae405e4111301051604c5d9d08465dd4c5d
mromero <maria.romero@news.co.uk>
2019-06-18 16:26:38 +0100

    A4: getting a response from the FT APIgit add .

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 const axios = require('axios');
 app.use(express.urlencoded());
 
-const port = 3000
+const port = 3000;
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
@@ -16,14 +16,20 @@ app.post('/submit-form', (req, res) => {
 
   const callConfig = {
     headers: {
-      'X-Api-Key':'',
-      'Content-Type':'application/json'
+      'X-Api-Key': '59cbaf20e3e06d3565778e7bde0b7e265f5844ddb68d827a9f3b7879',
+      'Content-Type': 'application/json',
+      'Accept': '*/*',
+      'Connection': 'keep-alive',
+      'cache-control': 'no-cache'
     },
-    queryString: article,
+  };
+  const body = { 
+    queryString: article
   };
 
-  axios.post(url, callConfig)
+  axios.post(url, body, callConfig)
     .then(function (response) {
+      console.log("got response");
       console.log(response.data);
     })
     .catch(function (error) {
